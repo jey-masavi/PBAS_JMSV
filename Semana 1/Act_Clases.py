f67047f5145bece93571b1190b0fb0b51911236b
jey-masavi <jessica.sanchezv@uanl.edu.mx>
2025-01-24 22:20:38 -0600

    Created

---
+++ Semana 1/Act_Clases.py
@@ -0,0 +1,12 @@
+#Actividad 1.
+print ("Hola mundo")
+
+#Actividad 2.
+
+n1 = int(imput("Ingrese el primer número:"))
+n2 = int(imput("Ingrese el segundo número:"))
+
+print("adición = ",n1 + n2)
+print("sustracción = ",n1 - n2)
+print("multiplicación = ",n1 * n2)
+print("división = ",n1 / n2)
