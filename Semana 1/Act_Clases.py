520448994cb97f53096c39d56bdcd7c306ff6549
jey-masavi <jessica.sanchezv@uanl.edu.mx>
2025-01-26 16:45:51 -0600

    Create Lab_Semana1.py

--- Semana 1/Act_Clases.py
+++ Semana 1/Act_Clases.py
@@ -3,8 +3,8 @@ print ("Hola mundo")
 
 #Actividad 2.
 
-n1 = int(imput("Ingrese el primer número:"))
-n2 = int(imput("Ingrese el segundo número:"))
+n1 = int(input("Ingrese el primer número:"))
+n2 = int(input("Ingrese el segundo número:"))
 
 print("adición = ",n1 + n2)
 print("sustracción = ",n1 - n2)
---
+++ Semana 1/Lab_Semana1.py
@@ -0,0 +1,62 @@
+# Laboratorio Semana 1
+
+print("Ejercicio 1: ¿Cúal es la función de la memoria secundaria en una computadora?")
+answer1= "c) Almacenar información durante mucho tiempo, incluso después de ciclos de apagado y encendido."
+print(answer1)
+
+print("Ejercicio 2: ¿Qué es un programa?")
+answer2= "Una secuencia de declaraciones o sentencias que ha sido diseñada para hacer algo."
+print(answer2)
+
+print("Ejercicio 3: ¿Cuál es la diferencia entre un compilador y un intérprete?")
+answer3= """El intérprete es un programa que lee y ejecuta el código fuente línea por línea, mientras que el
+compilador es un programa que traduce todo el código fuente a un lenguaje de máquina antes de su ejecución."""
+print(answer3)
+
+print("Ejercicio 4: ¿Cuál de los siguientes contiene código máquina?")
+answer4= "a) El intérprete de Python."
+print(answer4)
+
+print("Ejercicio 5: ¿Qué está mal en el siguiente código?:")
+answer5= "El comando print está mal escrito"
+print(answer5)
+
+print("Ejercicio 6: ¿En qué lugar del computador queda almacenada una variable, como en este caso “X”, después de ejecutar la siguiente línea de Python?")
+answer6= "b) Memoria Principal."
+print(answer6)
+
+print("Ejercicio 7: ¿Qué mostrará en pantalla el siguiente programa?")
+answer7= "b) 44"
+print(answer7)
+
+question8= """Ejercicio 8: Explica cada uno de los siguientes conceptos usando un ejemplo de una capacidad humana:
+(1) Unidad central de procesamiento, (2) Memoria prin￾cipal, (3) Memoria secundaria, (4) Dispositivos de entrada, y
+(5) Dispositivos de salida. Por ejemplo, “¿Cuál sería el equivalente humano de la Unidad central de procesamiento?”"""
+print(question8)
+answer8= """(1) Al igual que la CPU, es el cerebro de la computadora, controlando todas las operaciones y decisiones,
+el cerebro humano realiza la función de procesar información, tomar decisiones y coordinar las acciones de nuestro
+cuerpo. Por ejemplo, cuando necesitas resolver un problema matemático, es el cerebro el que procesa las operaciones
+mentales y toma las decisiones necesarias.
+
+(2) La memora principal de una computadora guarda datos temporalmente mientras el programa esta en ejecución, permitiendo
+que el procesador acceda rápidamente a la información que necesita. En el caso humano, la memoria a corto plazo es donde
+guardamos temporalmente la información que estamos utilizando en ese momento. Por ejemplo, si tienes que recordar un número
+de teléfono momentáneamente mientras lo marcas.
+
+(3) La memoria secundaria es donde se almacenan los datos de manera permanente o semi-permanente. Esta memoria es más
+lenta para acceder en comparación con la memoria principal, pero permite guardar información a largo plazo. El equivalente
+humano sería la memoria a largo plazo, donde almacenamos recuerdos, aprendizajes y experiencias.
+
+(4) Los dispositivos de entrada en una computadora permiten ingresar datos a la computadora. De forma análoga, en los humanos,
+nuestros sentidos, como la vista, el oído, el tacto, etc., funcionan como dispositivos de entrada. Por ejemplo, cuando ves
+una señal de tráfico o escuchas una conversación, estas recibiendo información a través de tus sentidos.
+
+(5) Los dispositivos de salida en una computadora permiten que la computadora muestre o emita resultados al usuario. El
+equivalente humano sería la boca, las manos o cualquier parte del cuerpo que utilizamos para salir con una respuesta o acción.
+Por ejemplo, si hablas para dar una respuesta a alguien o si escribes en un cuaderno."""
+print(answer8)
+
+print("Ejercicio 9: ¿Cómo puedes corregir un Error de sintaxis?")
+answer9= """Leyendo el mensaje de error, revisando paréntesis, corchetes y comillas, comprobando la indentación, verificando
+palabras clave y nombres de variables, comprobando operadores y separadores, corrigiendo el uso de funciones, etc."""
+print(answer9)
