f15096c307f1372b3def82c7bee6cc0e320d0200
jey-masavi <jessica.sanchezv@uanl.edu.mx>
2025-04-19 22:30:11 -0600

    Modified US to MX

--- PIA/main.py
+++ PIA/main.py
@@ -58,7 +58,7 @@ def search_for_artist(token, artist_name):
 
 # Obtener canciones populares
 def get_songs_by_artist(token, artist_id):
-    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=US"
+    url = f"https://api.spotify.com/v1/artists/{artist_id}/top-tracks?country=MX"
     headers = get_auth_header(token)
     result = get(url, headers=headers)
     json_result = json.loads(result.content)["tracks"]
